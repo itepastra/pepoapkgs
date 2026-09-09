f184e074287219d68ac529abd6b7b2586cfd9f96
Noa Aarts <noa@voorwaarts.nl>
2026-09-09 12:15:30 +0200

    move circular imports to extraAttrs so prev is explicit

--- modifications/default.nix
+++ modifications/default.nix
@@ -5,10 +5,16 @@ let
       entries = builtins.readDir ./.;
     in
     builtins.filter (name: entries.${name} == "directory") (builtins.attrNames entries);
+
+  extraAttrs = {
+    btop = {
+      btop = pkgs.btop;
+    };
+  };
 in
 builtins.listToAttrs (
   builtins.map (name: {
     inherit name;
-    value = pkgs.callPackage (./. + "/${name}") { };
+    value = pkgs.callPackage (./. + "/${name}") (extraAttrs.${name} or { });
   }) packageNames
 )
