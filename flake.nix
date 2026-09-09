cf6f8bbe88c9bf2958a3ca5ccfc29f9e0484bf0a
Noa Aarts <noa@voorwaarts.nl>
2026-09-09 14:00:52 +0200

    add home-manager to inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     { nixpkgs, ... }:
