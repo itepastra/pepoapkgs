52c6eea102a268b0a9fca9a441d68dd3c66e6a9e
Noa Aarts <noa@voorwaarts.nl>
2026-09-09 11:25:44 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      # Supported systems for your flake packages, shell, etc.
+      systems = [
+        "aarch64-linux"
+        # "i686-linux"
+        "x86_64-linux"
+        # "aarch64-darwin"
+        # "x86_64-darwin"
+      ];
+      # This is a function that generates an attribute by calling a function you
+      # pass to it, with each system as an argument
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system: import ./packages nixpkgs.legacyPackages.${system});
+    };
+}
